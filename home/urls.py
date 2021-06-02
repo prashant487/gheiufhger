4b4edec23eb99ee2d5b797c9e6965eb0df33fa39
prashant487 <78904858+prashant487@users.noreply.github.com>
2021-06-02 13:56:04 +0545

    first commit

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,20 @@ from django.urls import path, include
 from .views import *
 app_name = "home"
 urlpatterns = [
-    path('', homes, name='home')
+    path('', homes, name='home'),
+    path('contact', contact, name='contact'),
+    path('elements', elements, name='elements'),
+    path('portfolio', portfolio, name='portfolio'),
+    path('price', price, name='price'),
+    path('services', services, name='services'),
+    path('blog-home', blog_home, name='blog_home'),
+    path('blog-single', blog_single, name='blog_single'),
+    path('about', about, name='about'),
+
+
 ]
+
+
+
+
+
